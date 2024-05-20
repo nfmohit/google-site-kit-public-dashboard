5ad0c226269ebe9d9d57413b2d98f15222548d2e
Hussain Thajutheen <hussain.thajutheen@get10up.com>
2024-05-20 20:03:37 +0530

    Remove header and footer and load necessary functions from the template page.

--- includes/page-templates/page-public-dashboard.php
+++ includes/page-templates/page-public-dashboard.php
@@ -2,10 +2,32 @@
 /**
  * Template Name: Site Kit Public Dashboard
  */
-get_header();
+
+// Load necessary WordPress core files and functions.
+define( 'WP_USE_THEMES', false );
+require_once( ABSPATH . 'wp-load.php' );
+require_once( ABSPATH . WPINC . '/template-loader.php' );
+
+// Output the necessary parts of the header.
 ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<div id="content">
+
 <div class="googlesitekit-plugin">
-	<div id="js-googlesitekit-public-dashboard" data-view-only="true"></div>
+    <div id="js-googlesitekit-public-dashboard" data-view-only="true"></div>
 </div>
+
 <?php
-get_footer();
+// Output the necessary parts of the footer.
+?>
+</div> <!-- End of #content -->
+<?php wp_footer(); ?>
+</body>
+</html>
