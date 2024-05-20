24533679adca082afc159986f216bc84b0a0259e
nfmohit <hello@nahid.dev>
2024-05-20 16:54:39 +0600

    Use dynamic route.

--- includes/Public_Dashboard.php
+++ includes/Public_Dashboard.php
@@ -5,25 +5,34 @@ namespace Google\Site_Kit_Public_Dashboard;
 class Public_Dashboard {
 
 	public function register() {
-		add_filter( 'theme_page_templates', array( $this, 'add_page_template' ) );
-		add_filter('template_include', array( $this, 'load_page_template' ) );
+		add_action( 'init', array( $this, 'register_public_dashboard' ) );
+		add_filter( 'query_vars', array( $this, 'add_query_vars' ) );
+		add_filter( 'template_include', array( $this, 'load_template' ) );
 	}
 
-	public function load_page_template( $template ) {
-		if ( is_page_template( 'page-public-dashboard.php' ) ) {
-			$public_dashboard_template = GOOGLESITEKITPUBLICDASHBOARD_PLUGIN_DIR_PATH . 'includes/page-templates/page-public-dashboard.php';
+	public function register_public_dashboard() {
+		add_rewrite_rule(
+			'^google-site-kit/?',
+			'index.php?custom_page=google-site-kit',
+			'top'
+		);
+	}
 
-			if ( file_exists( $public_dashboard_template ) ) {
-				return $public_dashboard_template;
+	public function add_query_vars( $vars ) {
+		$vars[] = 'custom_page';
+
+		return $vars;
+	}
+
+	public function load_template( $template ) {
+		if ( get_query_var( 'custom_page' ) == 'google-site-kit' ) {
+			$template_path = GOOGLESITEKITPUBLICDASHBOARD_PLUGIN_DIR_PATH . 'includes/page-templates/page-public-dashboard.php';
+
+			if ( file_exists( $template_path ) ) {
+				return $template_path;
 			}
 		}
 
 		return $template;
-	}
-
-	public function add_page_template( $templates ) {
-		$templates['page-public-dashboard.php'] = __( 'Site Kit Public Dashboard' );
-
-    	return $templates;
 	}
 }
